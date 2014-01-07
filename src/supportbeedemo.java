a80ed400ba6b870d268c06732b2b71f9f814511f
Anirudh Bhat <abhat38@gmail.com>
2014-01-07 23:27:32 +0530

    Initial commit

---
+++ src/supportbeedemo.java
@@ -0,0 +1,22 @@
+
+public class supportbeedemo {
+	public static void main(String[] args){
+		SupportBee sb = new SupportBee("abhat", "1Ma2adYzfMyg9tLX5nzn");
+		System.out.println(sb.create_ticket("This is a Test subject", "test@gmail.com", "Yet another Demo Content"));
+		//System.out.println(sb.show_tickets("1882744"));
+		//System.out.println(sb.archive_ticket("1882744"));
+		//System.out.println(sb.unarchive_ticket("1882744"));
+		//System.out.println(sb.star_ticket("1882744"));
+		//System.out.println(sb.unstar_ticket("1882744"));
+		//System.out.println(sb.spam_ticket("1882744"));
+		//System.out.println(sb.unspam_ticket("1882744"));
+		//System.out.println(sb.untrash_ticket("1882744"));
+		//System.out.println(sb.fetch_reply_ticket("1882744"));
+		//System.out.println(sb.show_reply_ticket("1882744", "1"));
+		//System.out.println(sb.fetch_comment_ticket("1882744"));
+		//System.out.println(sb.fetch_agents(true));
+		//System.out.println(sb.fetch_labels());
+		//System.out.println(sb.add_labels("1882744", "testlabelname"));
+		//System.out.println(sb.remove_labels("1882744", "important"));
+	}
+}
